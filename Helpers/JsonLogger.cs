5c4e80e9b7704569dac087f9e7b54a570ef007fa
Karel Martyns <martynskarel@gmail.com>
2026-05-05 23:53:44 +0300

    Completed the helpers, but more explanations needed.

--- Helpers/JsonLogger.cs
+++ Helpers/JsonLogger.cs
@@ -1,1 +1,29 @@
-CSCSCS
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace ResilientWeatherGateway_Backend_Practice_2.Helpers
+{
+    public static class JsonLogger
+    {
+        private static readonly string LogFilePath = "CryptoPriceAggregator.log";
+
+        public static async Task LogAsync(object data)
+        {
+            try
+            {
+                string jsonline = JsonSerializer.Serialize(data);
+                await File.AppendAllTextAsync(LogFilePath, jsonline + Environment.NewLine);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"JSON.Logging failed : {ex.Message}");
+
+            }
+
+        }
+    }
+}
+
